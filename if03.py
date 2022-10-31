fd694998d0b54824c6becc5a372648f7d5ed9012
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-10-31 10:58:55 +0300

    03 done

--- if03.py
+++ if03.py
@@ -8,4 +8,35 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a > b > c :
+        return b
+    elif a > c > b :
+        return c
+    
+    elif b > c > a :
+         return c
+    elif b > a >c:
+         return a
+    
+    elif c > b > a :
+         return b
+    elif c > a > b:
+         return a
+    
+    elif a >= b > c:
+        return b
+    elif a >= c > b:
+        return a
+    
+    elif b >= a > c:
+        return a
+    elif b >= c > a:
+        return c
+    
+    elif c >= a > a:
+        return a
+    elif c >= b > a:
+        return b
+    
+print(main(3,7,1))
+print(main(5,5,-1))

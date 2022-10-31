ae6babc5a4c3fe112797c4578aafe39266433e2a
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-10-31 10:44:23 +0300

    01 done

--- if01.py
+++ if01.py
@@ -10,9 +10,17 @@ def main(a,b,c):
     """
     if a > b > c :
         return a
+    elif a > c > b :
+        return a
+    
     elif b > c > a :
          return b
+    elif b > a >c:
+         return b
+    
     elif c > b > a :
+         return c
+    elif c > a > b:
          return c
 print(main(1,4,2))
 print(main(-5,-3,-1))

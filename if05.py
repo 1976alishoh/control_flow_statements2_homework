1b96527286d01ce803d78dec51526c1b747ca64b
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-10-31 11:38:02 +0300

    05 done

--- if05.py
+++ if05.py
@@ -8,15 +8,15 @@ def main(n):
     """
     a = str(n)
     
-    if max(a)==a[0]:    
-         return a[0]
+    if max(a)==(a[0]):    
+         return int(a[0])
     elif max(a)==a[1]:    
-         return a[1]
+         return int(a[1])
     elif max(a)==a[2]:    
-         return a[2]
+         return int(a[2])
     elif max(a)==a[3]:    
-         return a[3]
+         return int(a[3])
     elif max(a)==a[4]:    
-         return a[4]
+         return int(a[4])
 
 print(main(23546))
